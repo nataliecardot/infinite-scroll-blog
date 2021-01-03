f8c9b87a082b781f6cce4134f3b9fdadd8983de9
Natalie-Cardot <nataliecardot@gmail.com>
2021-01-03 13:05:28 -0800

    Fetch posts

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-# Infinite Scroll Blog
+const postsContainer = document.getElementById('posts-container');
+const loading = document.querySelector('.loader');
+const filter = document.getElementById('filter');
+
+let limit = 5;
+let page = 1;
+
+// Fetch posts from API
+async function getPosts() {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts?_limit=${limit}&_page=${page}`
+  );
+
+  const data = await res.json();
+
+  // Note data returned is also a promise; use async/await
+  return data;
+}
