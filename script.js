5862733b0b36733611640988c7444d52004eef76
Natalie-Cardot <nataliecardot@gmail.com>
2021-01-06 18:30:37 -0800

    Add infinite scrolling

--- script.js
+++ script.js
@@ -36,5 +36,32 @@ async function showPosts() {
   });
 }
 
+// Show loader and fetch more posts
+function showLoading() {
+  loading.classList.add('show');
+
+  // So, after half a second, class is removed so animated loader dots have opacity set to 0, then after .3 seconds, the next page of posts are fetched
+  setTimeout(() => {
+    loading.classList.remove('show');
+
+    setTimeout(() => {
+      page++;
+      showPosts();
+    }, 300);
+  }, 500);
+}
+
 // Display initial posts
 showPosts();
+
+window.addEventListener('scroll', () => {
+  // scrollTop: number of pixels an element's content is scrolled vertically
+  // clientHeight: viewable height of an element in pixels, including padding, but not border, scrollbar or margin
+  // scrollHeight: entire height of an element in pixels, including padding, but not border, scrollbar or margin - including content not visible on screen
+  // Document.documentElement returns the Element that is the root element of the document (for HTML documents, this is the <html> element)
+  const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+
+  if (scrollTop + clientHeight >= scrollHeight - 5) {
+    showLoading();
+  }
+});
